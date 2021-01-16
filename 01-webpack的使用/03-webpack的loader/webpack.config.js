dc6e6fad0ee605049fedee472a8962786f7e3c5a
zhanlinghan <zhan@19921120@163.com>
2021-01-16 15:18:21 +0800

    webpack打包图片

--- 01-webpack的使用/03-webpack的loader/webpack.config.js
+++ 01-webpack的使用/03-webpack的loader/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'dist/'
     },
     module: {
         rules: [
@@ -15,7 +16,31 @@ module.exports = {
                 // style-loader负责将样式添加到DOM上
                 // 使用多个loader时，是从右向左
                 use: ['style-loader', 'css-loader']
-            }
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "less-loader"
+                }]
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            // 当加载的图片，小于limit时，会将图片编译成base64字符串形式
+                            // 当加载的图片，大于limit时，需要使用file-loader模块进行编译
+                            limit: 8196,
+                            name: 'img/[name].[hash:8].[ext]'
+                        },
+                    },
+                ],
+            },
         ]
     }
 }
