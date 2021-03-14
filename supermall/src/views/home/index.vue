1a617e209b815b76c202de99545486532486d10b
DavidBeckhamyes <zhan19921120@163.com>
2021-03-14 23:12:02 +0800

    添加轮播图准备

--- supermall/src/views/home/index.vue
+++ supermall/src/views/home/index.vue
@@ -1,12 +1,50 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+        <div>购物街</div>
+      </template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+// import { onMounted } from "vue";
+
 export default {
   name: "home",
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      banners: []
+    };
+  },
+  // Options API
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then((res) => {
+      this.banners = res.banner.list
+      console.log(this.banners);
+    });
+  },
+
+  // vue3.0方式
+  // setup() {
+  //   // Composition API
+  //   onMounted(() => {
+  //     console.log('++++++ mounted 2')
+  //   })
+  // }
 };
 </script>
 
 <style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #ffffff;
+}
 </style>
