d9cbf121097fe12cfd406cfddba81fe7bbe7b07a
DavidBeckhamyes <zhan19921120@163.com>
2021-03-21 14:25:53 +0800

    添加轮播图

--- supermall/src/views/home/index.vue
+++ supermall/src/views/home/index.vue
@@ -5,29 +5,40 @@
         <div>购物街</div>
       </template>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
 import { getHomeMultidata } from "network/home";
+import HomeSwiper from "./childComponents/HomeSwiper";
+import RecommendView from "./childComponents/RecommendView";
+import FeatureView from "./childComponents/FeatureView"
 // import { onMounted } from "vue";
 
 export default {
   name: "home",
   components: {
     NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
-      banners: []
+      banners: [],
+      recommends: []
     };
   },
   // Options API
   created() {
     // 1.请求多个数据
     getHomeMultidata().then((res) => {
-      this.banners = res.banner.list
+      this.banners = res.banner.list;
+      this.recommends = res.recommend.list
       console.log(this.banners);
     });
   },
@@ -43,8 +54,16 @@ export default {
 </script>
 
 <style scoped>
+ #home {
+   padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: #ffffff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
 }
 </style>
