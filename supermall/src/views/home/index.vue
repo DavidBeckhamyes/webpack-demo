b04e3d2abd7b1349119f9f6beefacbe30a9cfbb2
DavidBeckhamyes <zhan19921120@163.com>
2021-03-21 20:18:41 +0800

    tab生成

--- supermall/src/views/home/index.vue
+++ supermall/src/views/home/index.vue
@@ -8,15 +8,70 @@
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"></tab-control>
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
 import { getHomeMultidata } from "network/home";
 import HomeSwiper from "./childComponents/HomeSwiper";
 import RecommendView from "./childComponents/RecommendView";
-import FeatureView from "./childComponents/FeatureView"
+import FeatureView from "./childComponents/FeatureView";
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
 // import { onMounted } from "vue";
 
 export default {
@@ -25,12 +80,13 @@ export default {
     NavBar,
     HomeSwiper,
     RecommendView,
-    FeatureView
+    FeatureView,
+    TabControl,
   },
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
     };
   },
   // Options API
@@ -38,7 +94,7 @@ export default {
     // 1.请求多个数据
     getHomeMultidata().then((res) => {
       this.banners = res.banner.list;
-      this.recommends = res.recommend.list
+      this.recommends = res.recommend.list;
       console.log(this.banners);
     });
   },
@@ -54,8 +110,8 @@ export default {
 </script>
 
 <style scoped>
- #home {
-   padding-top: 44px;
+#home {
+  padding-top: 44px;
 }
 .home-nav {
   background-color: var(--color-tint);
@@ -65,5 +121,10 @@ export default {
   right: 0;
   top: 0;
   z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
