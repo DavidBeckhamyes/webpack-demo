53d442e4e497807ab06a092cb21d0e6fda092f88
DavidBeckhamyes <zhan19921120@163.com>
2021-11-07 14:08:08 +0800

    webpack增加注释

--- 01-webpack的使用/03-webpack的loader/webpack.config.js
+++ 01-webpack的使用/03-webpack的loader/webpack.config.js
@@ -1,17 +1,22 @@
 const path = require('path')
 
 module.exports = {
+    // 入口起点
     entry: './src/main.js',
     output: {
+        // 输出路径
+        // __dirname nodejs的变量，代表当前文件的目录绝对路径
         path: path.resolve(__dirname, 'dist'),
+        // 输出文件名
         filename: 'bundle.js',
         publicPath: 'dist/'
     },
+    // loader的配置
     module: {
         rules: [
             {
-
                 test: /\.css$/,
+                // 要使用多个loader处理用use
                 // css-loader只负责将css文件进行加载
                 // style-loader负责将样式添加到DOM上
                 // 使用多个loader时，是从右向左
@@ -20,20 +25,27 @@ module.exports = {
             {
                 test: /\.less$/,
                 use: [{
+                    // 创建style标签，将js中的样式资源插入进去，添加到head中生效
                     loader: "style-loader"
-                }, {
+                }, 
+                {   
+                    // 将css文件变成commonjs模块加载到js中，里面的内容是样式字符串
                     loader: "css-loader"
-                }, {
+                }, 
+                {
                     loader: "less-loader"
                 }]
             },
-            {
+            {   
+                // 问题：默认处理不了html中的img图片
                 test: /\.(png|jpg|gif|jpeg)$/i,
                 use: [
                     {
                         loader: 'url-loader',
                         options: {
                             // 当加载的图片，小于limit时，会将图片编译成base64字符串形式
+                            // 优点：减少请求数量（减轻服务器压力）
+                            // 缺点：图片体积会更大（文件请求速度更慢）
                             // 当加载的图片，大于limit时，需要使用file-loader模块进行编译
                             limit: 8196,
                             name: 'img/[name].[hash:8].[ext]'
--- 01-webpack的使用/04-webpack配置vue/webpack.config.js
+++ 01-webpack的使用/04-webpack配置vue/webpack.config.js
@@ -19,7 +19,6 @@ module.exports = {
     module: {
         rules: [
             {
-
                 test: /\.css$/,
                 // css-loader只负责将css文件进行加载
                 // style-loader负责将样式添加到DOM上
@@ -44,11 +43,20 @@ module.exports = {
                         options: {
                             // 当加载的图片，小于limit时，会将图片编译成base64字符串形式
                             // 当加载的图片，大于limit时，需要使用file-loader模块进行编译
+                            // [hash:8]取图片的hash的前8位
                             limit: 8196,
                             name: 'img/[name].[hash:8].[ext]'
                         },
                     },
                 ],
+            },
+            {
+                test: /\.html$/,
+                // 处理html文件的img图片（负责引入img，从而能被url-loader进行打包处理）
+                // 问题：因为url-loader默认使用ES6模块化解析，而html-loader引入图片是commonjs
+                // 解析时会出现问题：[object Module]
+                // 解决：关闭url-loader的es6模块化，使用commonjs解析
+                loader: 'html-loader'
             },
             {
                 test: /\.m?js$/,
@@ -73,6 +81,9 @@ module.exports = {
         new VueLoaderPlugin(),
         new webpack.BannerPlugin('最终版权归ZLH所有'),
         new HtmlWebpackPlugin({
+            // 功能：默认会创建一个空的HTML，自动引入打包输出的所有资源（JS/CSS）
+            // 需求：需要有结构的HTML文件
+            // 复制 './index.html'文件，并自动引入打包输出的所有资源（JS/CSS）
             template: 'index.html'
         }),
         new UglifyjsWebpackPlugin()
