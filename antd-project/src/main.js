75adadfb71454683e838e5183da7e04c3c1cb08b
詹凌瀚 <zhanlinghan@rails.cn>
2022-01-25 17:23:56 +0800

    新增ant-design项目

---
+++ antd-project/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Antd from 'ant-design-vue'// 引入Ant Design Vue组件
+import 'ant-design-vue/dist/antd.css' // 引入Ant Design Vue样式
+createApp(App).use(Antd).mount('#app')
