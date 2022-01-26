1e6994c7ef806cf0bafd47a41d045eb4e4e9fa4b
詹凌瀚 <zhanlinghan@rails.cn>
2022-01-26 10:34:39 +0800

    构建框架

--- antd-project/src/main.js
+++ antd-project/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Antd from 'ant-design-vue'// 引入Ant Design Vue组件
-import 'ant-design-vue/dist/antd.css' // 引入Ant Design Vue样式
-createApp(App).use(Antd).mount('#app')
+import { Button } from 'ant-design-vue'// 引入Ant Design Vue组件
+// import 'ant-design-vue/dist/antd.css' // 引入Ant Design Vue样式
+createApp(App).use(Button).mount('#app')
